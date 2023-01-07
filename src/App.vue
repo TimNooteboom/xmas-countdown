dcfa06204265b3ff2fae63d584d6e81cee2c8370
Tim Nooteboom <tim.nooteboom@dhigroupinc.com>
2023-01-07 12:37:06 -0600

    refactored code

--- src/App.vue
+++ src/App.vue
@@ -3,52 +3,47 @@ import CountdownHeader from '@/components/CountdownHeader.vue'
 import CountdownSegment from './components/CountdownSegment.vue'
 import { useNow } from '@vueuse/core'
 import { computed } from 'vue'
-const now = useNow()
+const now = useNow() // useNow gives us the reactive time for every second without having to call setInterval
+const year = now.value.getFullYear()
 
-const christmas = new Date('12/25/2022 00:00:00')
+const christmas = new Date(year, 11, 25) // find Christmas for the given year
+const one_day = 1000 * 60 * 60 * 24
+const one_hour = 1000 * 60 * 60
+const one_minute = 1000 * 60
+const one_second = 1000
+
+const distance = computed(() => {
+  return christmas.getTime() - now.value.getTime()
+})
 
 const days = computed(() => {
-  const one_day = 1000 * 60 * 60 * 24
-  return (christmas.getTime() - now.value.getTime()) / one_day
-})
-const daysRounded = computed(() => {
-  return Math.floor(days.value)
+  return Math.floor(distance.value / one_day)
 })
 
 const hours = computed(() => {
-  return 24 * (days.value - daysRounded.value)
-})
-
-const hoursRounded = computed(() => {
-  return Math.floor(hours.value)
+  return Math.floor((distance.value % one_day) / one_hour)
 })
 
 const minutes = computed(() => {
-  return 60 * (hours.value - hoursRounded.value)
-})
-
-const minutesRounded = computed(() => {
-  return Math.floor(minutes.value)
+  return Math.floor((distance.value % one_hour) / one_minute)
 })
 
 const seconds = computed(() => {
-  return 60 * (minutes.value - minutesRounded.value)
+  return Math.floor((distance.value % one_minute) / one_second)
 })
 
-const secondsRounded = computed(() => {
-  return Math.floor(seconds.value)
-})
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
     <div>
+      {{now}}
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" :number="daysRounded" />
-          <CountdownSegment label="hours" :number="hoursRounded" />
-          <CountdownSegment label="minutes" :number="minutesRounded" />
-          <CountdownSegment label="seconds" :number="secondsRounded" />
+          <CountdownSegment label="days" :number="days" />
+          <CountdownSegment label="hours" :number="hours" />
+          <CountdownSegment label="minutes" :number="minutes" />
+          <CountdownSegment label="seconds" :number="seconds" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
--- src/components/CountdownSegment.vue
+++ src/components/CountdownSegment.vue
@@ -1,9 +1,10 @@
 <script setup>
-defineProps({
-  label: String,
-  number: Number,
-})
+  defineProps({
+    label: String,
+    number: Number,
+  })
 </script>
+
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
@@ -15,33 +16,30 @@ defineProps({
     <span class="label block pt-2">{{ label }}</span>
   </div>
 </template>
+
 <style>
-.segment {
-  width: 80px;
-}
-.numbers {
-  transform: translateX(-50%);
-  font-size: 32px;
-}
-.label {
-  font-size: 16px;
-}
-.v-enter-active,
-.v-leave-active {
-  transition: all 0.5s ease;
-}
-.v-enter-from {
-  transform: translateY(-100%) translateX(-50%);
-}
-.v-leave-to {
-  transform: translateY(100%) translateX(-50%);
-}
-.v-enter-to,
-.v-leave-from {
-  transform: translateY(0px) translateX(-50%);
-}
-.v-enter-from,
-.v-leave-to {
-  /* opacity: 0; */
-}
+  .segment {
+    width: 80px;
+  }
+  .numbers {
+    transform: translateX(-50%);
+    font-size: 32px;
+  }
+  .label {
+    font-size: 16px;
+  }
+  .v-enter-active,
+  .v-leave-active {
+    transition: all 0.5s ease;
+  }
+  .v-enter-from {
+    transform: translateY(-100%) translateX(-50%);
+  }
+  .v-leave-to {
+    transform: translateY(100%) translateX(-50%);
+  }
+  .v-enter-to,
+  .v-leave-from {
+    transform: translateY(0px) translateX(-50%);
+  }
 </style>
